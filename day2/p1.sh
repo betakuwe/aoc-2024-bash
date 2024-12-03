d4ac83c20e2537199b1cfe92cdc71730f8537c43
Benjamin Tan <benjamin.tan@klasses.com.sg>
2024-12-03 11:48:45 +0800

    d2p1

--- day2/p1.sh
+++ day2/p1.sh
@@ -1,19 +1,34 @@
-cat "$1"
-printf "\n"
+FILE="input.txt"
 
 check_within_range() {
-	sed <<<"$1" -e 's/-//' -e 's/\(^.*$\)/1<=\1\&\&\1<=3/' | bc
+	while IFS= read -r diff; do
+		abs_diff=${diff//-/}
+		if [[ $abs_diff -lt 1 || $abs_diff -gt 3 ]]; then
+			local res=0
+			break
+		fi
+	done <<<"$1"
+	printf "%s" "${res:=1}"
+
+	# works, but slower by 1sec
+	# sed <<<"$1" -e 's/-//' -e 's/\(^.*$\)/1<=\1\&\&\1<=3/' |
+	# 	paste -sd'~' |
+	# 	sed 's/~/\&\&/g' |
+	# 	bc
 }
 
 while IFS= read -r report; do
 	report_lines=$(tr ' ' '\n' <<<"$report")
-	diffs=$(paste -d- <(head -n-1 <<<"$report_lines") <(tail -n+2 <<<"$report_lines") |
+	heads=$(head -n-1 <<<"$report_lines")
+	diffs=$(paste -d- \
+		<(printf '%s' "$heads") \
+		<(tail -n+2 <<<"$report_lines") |
 		bc)
-	printf "%s\n" "$diffs"
 	minus_counts=$(grep -c '-' <<<"$diffs")
-	if [[ "$minus_counts" -ne 0 && "$minus_counts" -ne 4 || $(check_within_range "$diffs") =~ 0 ]]; then
-		cat <<<no
-	fi
-	# echo "$minus_counts"
-	printf "\n"
-done <"$1"
+	# echo $diffs
+	# check_within_range $diffs
+	[[ "$minus_counts" -eq 0 || "$minus_counts" -eq $(wc -l <<<"$heads") ]] &&
+		[[ $(check_within_range "$diffs") -eq 1 ]] &&
+		cat <<<h
+done <"$FILE" |
+	wc -l
