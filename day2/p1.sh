9208afc5b2f68123d01161baa56129281fc0cd3b
Benjamin Tan <benjamin.tan@klasses.com.sg>
2024-12-03 18:48:46 +0800

    made d2p1 much faster

--- day2/p1.sh
+++ day2/p1.sh
@@ -1,27 +1,29 @@
 FILE="input.txt"
 
-check_within_range() {
+check_safety() {
+	local prev_sign sign
 	while IFS= read -r diff; do
+		[[ $diff =~ - ]] && sign=- || sign=+
+		if [[ $sign != "${prev_sign:=$sign}" ]]; then
+			false
+			return
+		fi
 		local abs_diff=${diff//-/}
 		if [[ $abs_diff -lt 1 || $abs_diff -gt 3 ]]; then
 			false
 			return
 		fi
-	done <<<"$1"
+	done
 }
 
 while IFS= read -r report; do
-	report_lines=$(tr ' ' '\n' <<<"$report")
-	heads=$(head -n-1 <<<"$report_lines")
-	diffs=$(paste -d- \
-		<(printf '%s' "$heads") \
-		<(tail -n+2 <<<"$report_lines") |
-		bc)
-	minus_counts=$(tr -cd '-' <<<"$diffs" | wc -c)
-	# minus_counts=$(grep -c '-' <<<"$diffs") # works but a little slower
-	[[ "$minus_counts" -eq 0 || "$minus_counts" -eq $(wc -l <<<"$heads") ]] &&
-		check_within_range "$diffs" &&
-		((safe++))
+	sed <<<"$report" \
+		-e 's/ \([0-9]\+\)/-\1)(\1/g' \
+		-e 's/^\(.*\)([0-9]\+/(\1/' |
+		while IFS= read -r line; do
+			bc <<<"${line//)(/)$'\n'(}"
+		done |
+		check_safety && ((safe++))
 done <"$FILE"
 
 printf '%s' "$safe"
